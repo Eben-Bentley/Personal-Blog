c7a67d69077db99f24343ceb97155b1148511d8f
Eben B <eben.bentley@gmail.com>
2026-01-13 22:57:09 +0000

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://ebenbentley.dev',
   output: 'static',
   build: {
     inlineStylesheets: 'auto',
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
